8ed2e04b3eeb143faeb52fba6ab33c99288e1a70
Giwon Lee <2giwon@gmail.com>
2024-06-06 14:12:56 +0900

    HardEdge, Make Card

--- lib/main.dart
+++ lib/main.dart
@@ -71,6 +71,93 @@ class UiChallengeApp extends StatelessWidget {
                     textColor: Colors.white),
               ],
             ),
+            const SizedBox(height: 100),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  "Wallets",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
+                Text(
+                  "View All",
+                  style: TextStyle(
+                    color: Colors.white.withOpacity(0.8),
+                    fontSize: 14,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Container(
+              clipBehavior: Clip.hardEdge,
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.elliptical(40, 40),
+                  bottomLeft: Radius.elliptical(40, 40),
+                  bottomRight: Radius.circular(20),
+                ),
+                color: Color(0xFF181818),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(24),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          "Euro",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        Row(
+                          children: [
+                            const Text(
+                              "6 428",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                              ),
+                            ),
+                            const SizedBox(width: 10),
+                            Text(
+                              "EUR",
+                              style: TextStyle(
+                                color: Colors.white.withOpacity(0.8),
+                                fontSize: 12,
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                    Transform.scale(
+                        scale: 2.2,
+                        child: Transform.translate(
+                          offset: const Offset(-5, 15),
+                          child: const Icon(
+                            Icons.euro_symbol,
+                            color: Colors.white,
+                            size: 80,
+                          ),
+                        )),
+                  ],
+                ),
+              ),
+            ),
           ]),
         ),
       ),
